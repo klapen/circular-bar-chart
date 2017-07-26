362fb1f63f7e9f21d298d9b5b19afe81907e5be9
klapen <oasaavedrac@gmail.com>
2017-07-26 13:49:31 -0500

    First commit

---
+++ src/js/circular-bar-chart.js
@@ -0,0 +1,14 @@
+var circularBarChart = {
+    generate: function(){
+	var graph = {
+	    loadData: function(){},
+	    updateData: function(){},
+	    render: function(){},
+	}
+	return graph
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    
+})
